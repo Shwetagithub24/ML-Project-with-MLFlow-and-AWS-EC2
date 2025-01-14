7915c690bca97dbf9f87cfa006bd2ec4e7791609
Shwetagithub24 <shwetazarekar6@gmail.com>
2025-01-14 22:27:43 +0530

    Installed requirements

---
+++ setup.py
@@ -0,0 +1,31 @@
+# Local Package installation
+# addes constructor file __init__.py to each folder so that we can use it in some other.
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ML-Project-with-MLFlow-and-AWS-EC2"
+AUTHOR_USER_NAME = "Shwetagithub24"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "shwetazarekar6@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
